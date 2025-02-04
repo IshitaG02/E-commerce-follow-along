a1f9d58e573771719fa19541e574322275b02a52
IshitaG02 <Ishita.Gupta@kalvium.community>
2025-02-04 13:08:32 +0530

    Milestone 9 completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
+import React from 'react'
 import { BrowserRouter,Routes,Route } from "react-router-dom"; 
-import { Home ,  LoginPage, SignupPage } from "./Routes.jsx"
-
+import { Home , LoginPage, SignupPage, CreateProduct} from "./Routes.jsx"
 import './App.css';
 
-function App(){
+const App = () => {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<LoginPage/>}/>
-        <Route path="/Signup" element={<SignupPage/>}/>
-        <Route path="/" element={<Home/>}/>
-
+        <Route path="/Login" element={<LoginPage/>} />
+        <Route path="/Signup" element={<SignupPage/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/Create-product" element={<CreateProduct/>} />
       </Routes>
     </BrowserRouter>
   )
