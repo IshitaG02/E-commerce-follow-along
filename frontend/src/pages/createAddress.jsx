756337858cde0f8c976a75a29afa79b231f1d289
IshitaG02 <Ishita.Gupta@kalvium.community>
2025-02-28 14:42:34 +0530

    Milestone 22 done

--- frontend/src/pages/createAddress.jsx
+++ frontend/src/pages/createAddress.jsx
@@ -19,7 +19,7 @@ const CreateAddress = () => {
             address2,
             zipCode,
             addressType,
-            email: "gowrishvarma@gmail.com"
+            email: "bkuvbwskj@gmail.com"
         };
         try {
             const response = await axios.post(
